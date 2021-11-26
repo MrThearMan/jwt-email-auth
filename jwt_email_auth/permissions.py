abbc478ffe171baf29c63d663d00400d5e63a4e0
Matti Lamppu <matti.lamppu@vincit.fi>
2021-11-26 23:22:35 +0200

    Allow viewing the schema if in debug mode

--- jwt_email_auth/permissions.py
+++ jwt_email_auth/permissions.py
@@ -17,6 +17,11 @@ logger = logging.getLogger(__name__)
 
 class HasValidJWT(BasePermission):
     def has_permission(self, request, view):
+
+        # Allow viewing the schema if in debug mode
+        if request.method == "OPTIONS" and settings.DEBUG:
+            return True
+
         try:
             AccessToken.from_request(request, expected_claims=auth_settings.EXPECTED_CLAIMS)
         except AuthenticationFailed as f:
